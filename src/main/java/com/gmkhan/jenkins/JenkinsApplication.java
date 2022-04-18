a37d89cb82caaa338d408e4b6ffe7a3734c4db9e
GMKHAN <GMKHAN@GMKHAN-7410.in.oracle.com>
2022-04-18 22:52:59 +0530

    first commit

---
+++ src/main/java/com/gmkhan/jenkins/JenkinsApplication.java
@@ -0,0 +1,25 @@
+package com.gmkhan.jenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(JenkinsApplication.class);
+
+	@PostConstruct
+	public void init(){
+		logger.info("Application started...");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executed...");
+		SpringApplication.run(JenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmkhan/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,25 @@
+package com.gmkhan.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(JenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads()
+	{
+		logger.info("Test case executing...");
+		assertEquals(true,true);
+	}
+
+}
